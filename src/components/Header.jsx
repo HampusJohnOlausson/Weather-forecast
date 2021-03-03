0330a59535e9b7c53215d6f2a62facfc0c8b6cf9
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-03-03 18:19:06 +0100

    added some styles to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import "../style/header.css";
 
 export default class Header extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
-        )
+          <header className="header">
+            <form>
+              <div className="search-container">
+                <input placeholder="search location..." className="input" />
+                <button className="search-btn"></button>
+              </div>
+            </form>
+          </header>
+        );
     }
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import Header from './Header'
 import Main from './Main'
-import '../style/header.css';
+
 
 export default class Layout extends Component {
     render() {
         return (
             <div>
-               <Header className="header"/> 
+               <Header/> 
                <Main/>
             </div>
         )
